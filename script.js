5750e7ff5d96d7a050195cd9d6b430a85ae6fd95
Mike Sargsyan <msargsya@stevens.edu>
2022-12-10 19:52:31 -0500

    Javascript
    ・3 main images on the main page now displays a brief explanation on mouse hover.
    
    ・Self introductions on Who Are We page are now invisible by default and can be revealed by clicking on students' name.
    
    ・Fixed indents for readability
    
    ・Fixed <div> inbalance.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-/* global document */
+///* global document */
+//
+$(function() {
+    $("#name1").click(function() {
+        $("#wwa1").fadeToggle();
+    });
+    
+    $("#name2").click(function() {
+        $("#wwa2").fadeToggle();
+    });
+    
+    $("#sensorama").hover(function () {
+        $(".sensoramaText").fadeToggle();
+        $(".sensoramaText").text("The syn-aesthetic, immersive machine developed by Heilig in 1962 widened the Expanded Cinema to a theater of total illusion, which Heilig called the «experience theater.»"); 
+        //http://www.medienkunstnetz.de/works/sensorama/
+    });
+    
+    $("#nasa").hover(function () {
+        $(".nasaText").fadeToggle();
+        $(".nasaText").text("The Virtual Interface Environment Workstation (VIEW), 1990"); 
+    });
+    
+    $("#vr90s").hover(function () {
+        $(".vr90sText").fadeToggle();
+        $(".vr90sText").text("Virtual reality equipment and games from the beginning of the 1990's."); 
+    });
+});
