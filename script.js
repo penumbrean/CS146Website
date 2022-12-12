0335416c1b38bb895fc6f7d296570e68a51ea584
chulsonv <116310822+chulsonv@users.noreply.github.com>
2022-12-12 01:13:51 -0500

    add dom

--- script.js
+++ script.js
@@ -33,3 +33,105 @@ $(function() {
         $(".vr90sText").text("Virtual reality equipment and games from the beginning of the 1990's."); 
     });
 });
+
+function page1(){
+    //1-7
+    document.getElementById("1").style.visibility = "visible";
+    document.getElementById("2").style.visibility ="visible";
+    document.getElementById("3").style.visibility ="visible";
+    document.getElementById("4").style.visibility ="visible";
+    document.getElementById("5").style.visibility ="visible";
+    document.getElementById("6").style.visibility ="visible";
+    document.getElementById("7").style.visibility ="visible";
+    document.getElementById("8").style.visibility ="hidden";
+    document.getElementById("9").style.visibility ="hidden";
+    document.getElementById("10").style.visibility ="hidden";
+    document.getElementById("11").style.visibility = "hidden";
+    document.getElementById("12").style.visibility ="hidden";
+    document.getElementById("13").style.visibility ="hidden";
+    document.getElementById("14").style.visibility ="hidden";
+    document.getElementById("15").style.visibility ="hidden";
+    document.getElementById("16").style.visibility ="hidden";
+    document.getElementById("17").style.visibility ="hidden";
+    document.getElementById("18").style.visibility ="hidden";
+    document.getElementById("19").style.visibility ="hidden";
+    document.getElementById("20").style.visibility ="hidden";
+    document.getElementById("21").style.visibility ="hidden";
+    document.getElementById("22").style.visibility ="hidden";
+}
+
+function page2(){
+    //8-12
+    document.getElementById("1").style.visibility = "hidden";
+    document.getElementById("2").style.visibility ="hidden";
+    document.getElementById("3").style.visibility ="hidden";
+    document.getElementById("4").style.visibility ="hidden";
+    document.getElementById("5").style.visibility ="hidden";
+    document.getElementById("6").style.visibility ="hidden";
+    document.getElementById("7").style.visibility ="hidden";
+    document.getElementById("8").style.visibility ="visible";
+    document.getElementById("9").style.visibility ="visible";
+    document.getElementById("10").style.visibility ="visible";
+    document.getElementById("11").style.visibility = "visible";
+    document.getElementById("12").style.visibility ="visible";
+    document.getElementById("13").style.visibility ="hidden";
+    document.getElementById("14").style.visibility ="hidden";
+    document.getElementById("15").style.visibility ="hidden";
+    document.getElementById("16").style.visibility ="hidden";
+    document.getElementById("17").style.visibility ="hidden";
+    document.getElementById("18").style.visibility ="hidden";
+    document.getElementById("19").style.visibility ="hidden";
+    document.getElementById("20").style.visibility ="hidden";
+    document.getElementById("21").style.visibility ="hidden";
+    document.getElementById("22").style.visibility ="hidden";
+}
+function page3(){
+    //13-17
+    document.getElementById("1").style.visibility = "hidden";
+    document.getElementById("2").style.visibility ="hidden";
+    document.getElementById("3").style.visibility ="hidden";
+    document.getElementById("4").style.visibility ="hidden";
+    document.getElementById("5").style.visibility ="hidden";
+    document.getElementById("6").style.visibility ="hidden";
+    document.getElementById("7").style.visibility ="hidden";
+    document.getElementById("8").style.visibility ="hidden";
+    document.getElementById("9").style.visibility ="hidden";
+    document.getElementById("10").style.visibility ="hidden";
+    document.getElementById("11").style.visibility = "hidden";
+    document.getElementById("12").style.visibility ="hidden";
+    document.getElementById("13").style.visibility ="visible";
+    document.getElementById("14").style.visibility ="visible";
+    document.getElementById("15").style.visibility ="visible";
+    document.getElementById("16").style.visibility ="visible";
+    document.getElementById("17").style.visibility ="visible";
+    document.getElementById("18").style.visibility ="hidden";
+    document.getElementById("19").style.visibility ="hidden";
+    document.getElementById("20").style.visibility ="hidden";
+    document.getElementById("21").style.visibility ="hidden";
+    document.getElementById("22").style.visibility ="hidden";
+}
+function page4(){
+    //18-22
+    document.getElementById("1").style.visibility = "hidden";
+    document.getElementById("2").style.visibility ="hidden";
+    document.getElementById("3").style.visibility ="hidden";
+    document.getElementById("4").style.visibility ="hidden";
+    document.getElementById("5").style.visibility ="hidden";
+    document.getElementById("6").style.visibility ="hidden";
+    document.getElementById("7").style.visibility ="hidden";
+    document.getElementById("8").style.visibility ="hidden";
+    document.getElementById("9").style.visibility ="hidden";
+    document.getElementById("10").style.visibility ="hidden";
+    document.getElementById("11").style.visibility = "hidden";
+    document.getElementById("12").style.visibility ="hidden";
+    document.getElementById("13").style.visibility ="hidden";
+    document.getElementById("14").style.visibility ="hidden";
+    document.getElementById("15").style.visibility ="hidden";
+    document.getElementById("16").style.visibility ="hidden";
+    document.getElementById("17").style.visibility ="hidden";
+    document.getElementById("18").style.visibility ="hidden";
+    document.getElementById("19").style.visibility ="visible";
+    document.getElementById("20").style.visibility ="visible";
+    document.getElementById("21").style.visibility ="visible";
+    document.getElementById("22").style.visibility ="visible";
+}
